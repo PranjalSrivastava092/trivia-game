c072d6e66f58625fa0bf0fa0f2c84c95651f633e
Pranjal Srivastava <pranjalsri092@gmail.com>
2020-03-28 15:19:27 +0530

    bug fixing

--- api/routes/router.js
+++ api/routes/router.js
@@ -4,60 +4,65 @@ const User = require('../models/User');
 const { check } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config');
+const request = require('request');
 const auth = require('../../auth');
 
 router.get('/user', auth, async function(request, response){
-  try {
-    const user = await User.findById(request.user.id);
-    response.send(user.select('-password'));
-  } catch (err) {
-    console.error(err.msg);
-    response.status(500).send('Server Error');
-  }
+  User.findById(request.user.id, function(err,user){
+    if(err){
+      console.error(err.msg);
+      response.status(500).send('Server Error');
+    }
+    else{
+      response.send(user.select('-password'));
+    }
+  });
 });
 
 router.post('/user/login',[ check('email', 'Please enter a valid email').isEmail(),
     check('password', 'The minimum length of password should be 8').isLength({ min: 8 })],
-  async function(request, response){
-    try {
+    async function(request, response){
       const { email, password } = request.body;
-      var doc = await User.findOne({ email: email });
+      User.findOne({ email: email }, function(err,doc){
+        if(err){
+          console.error(err.message);
+          response.send(400).send({ msg: 'server error' });
+        }
+	else{
+	  if (!doc) {
+            user = new User({
+              email: email,
+              password: password
+            });
 
-      if (!doc) {
-        user = new User({
-          email,
-          password
-        });
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            user.save(function(err){
+	      if(!err){
+                const token = jwt.sign({user: { id: user.id }}, 'SecretKey9999');
+                response.send({
+                  success: true,
+                  token: token
+                });
+	      }
+	    });
+          }
+	  else{
+	    const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+              return response.status(400).send({ errors: [{ msg: 'Password is incorrect' }] });
+            }
 
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
-        user.save(function(err){
-	  if(!err){
             const token = jwt.sign({user: { id: user.id }}, 'SecretKey9999');
-            return response.send({
+            response.send({
               success: true,
               token: token
             });
 	  }
-	});
-      }
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return response.status(400).send({ errors: [{ msg: 'Password is incorrect' }] });
-      }
-
-      const token = jwt.sign({user: { id: user.id }}, 'SecretKey9999');
-      response.send({
-        success: true,
-        token: token
+	}
       });
-    } 
-    catch (err) {
-      console.error(err.message);
-      response.send(400).send({ msg: 'server error' });
-    }
-  }
-);
+    });
+
+
 
 module.exports = router;
